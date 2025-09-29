c5827b9e601bcf3088e0d002a50f79d4ce8eda15
yagur <yagur@DESKTOP-HJ1O55H>
2025-09-30 00:40:49 +0300

    Teachers DONE

---
+++ BlazorAcademy/Models/Human.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAcademy.Models
+{
+    public class Human
+    {
+        [Required]
+        public string last_name { get; set; }
+        [Required]
+        public string first_name { get; set; }
+        public string? middle_name { get; set; }
+        [Required]
+        public DateOnly birth_date { get; set; }
+        [EmailAddress]
+        public string? email { get; set; }
+        [Phone]
+        public string? phone { get; set; }
+        public byte[]? photo { get; set; }
+    }
+}
---
+++ BlazorAcademy/Models/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorAcademy.Models
+{
+    public class Teacher : Human
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public short teacher_id { get; set; }
+        [Required]
+        public DateOnly work_since { get; set; }
+        [Required]
+        [Display(Name = "Ставка")]
+        public decimal rate { get; set; }
+    }
+}
